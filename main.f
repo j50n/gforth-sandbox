c2cf5dc91c559a3f5a66f00303f48313b54d3a5e
j50n <j50n@lilypepper.com>
2020-12-20 10:28:23 -0700

    more utility words

--- main.f
+++ main.f
@@ -29,3 +29,36 @@
 : show-cursor ( -- )
     27 emit
     ." [?25h" ;
+
+\ Draw a horizontal line of char ch from x,y with length len, draw direction right.
+: hline ( n1 n1 n1 n1 -- ) { x y len ch }
+    x y at-xy
+    len 0 ?do
+        ch emit
+    loop ;
+
+\ Draw a vertical line of char ch from x,y with length len, draw direction down.
+: vline ( n1 n1 n1 n1 -- ) { x y len ch }
+    len 0 ?do
+        x y i + at-xy
+        ch emit
+    loop ;
+
+\ Height of the terminal in characters.
+: term-height ( -- n1 )
+    form drop ;
+
+\ Width of the terminal in characters.
+: term-width ( -- n1 )
+    form nip ;
+
+\ Draws a box around the edges of the terminal. Useless, but proves I can
+\ do this with invisible cursor and without auto-scrolling.
+: box-border ( -- )
+    hide-cursor
+    0 0 term-width [char] @ hline                   ( line at top )
+    0 term-height 1 - term-width [char] @ hline     ( line at bottom )
+    0 0 term-height [char] @ vline                  ( line at left )
+    term-width 1 - 0 term-height [char] @ vline     ( line at right )
+    wait-for-escape
+    show-cursor ;
